e00b51a04c1b57f4dd61b408b14cdbd9e8e08ba1
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-12-25 20:56:02 +0800

    Initial Commit

---
+++ ZacharyApp/ZacharyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZacharyAppApp.swift
+//  ZacharyApp
+//
+//  Created by Zachary on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ZacharyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
