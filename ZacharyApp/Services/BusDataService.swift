341d0f971993896a1b0c1d48f90647eabd3bc371
ZacharyYongBB <zacharyyong_1997@outlook.com>
2023-01-02 23:10:50 +0800

    download coin image

--- ZacharyApp/Services/BusDataService.swift
+++ ZacharyApp/Services/BusDataService.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Combine
+
+//class BusDataService {
+//    
+//    @Published var busData
+//    
+//    var busSubscription: AnyCancellable?
+//    
+//    init() {
+//        getBusData()
+//    }
+//    
+//    private func getBusData(urlString: String) {
+//        guard let url = URL(string: urlString)
+//        else {return}
+//        
+//        busSubscription = NetworkingManager.download(url: url)
+//            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+//            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedCoins in
+//                self?.allCoin = returnedCoins
+//                // cancel after getting data, instead of stays listening in case of more published values in the future
+//                self?.coinSubscription?.cancel()
+//            })
+//    }
+//}
---
+++ ZacharyApp/Services/CoinImageService.swift
@@ -0,0 +1,38 @@
+//
+//  CoinImageService.swift
+//  ZacharyApp
+//
+//  Created by Zachary on 1/1/23.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    private var imageSubscription: AnyCancellable?
+    private let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinImage()
+    }
+    
+    private func getCoinImage() {
+        guard let url = URL(string: coin.image)
+        else {return}
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ data -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
+                self?.image = returnedImage
+                // cancel after getting data, instead of stays listening in case of more published values in the future
+                self?.imageSubscription?.cancel()
+            })
+    }
+}
